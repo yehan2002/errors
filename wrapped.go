b92afb240a6a313efc13fd044d6439b36552665f
yehan2002 <yehanjaya2002@gmail.com>
2020-12-20 16:09:08 +0530

    initial commit

---
+++ wrapped.go
@@ -0,0 +1,19 @@
+package errors
+
+type wrappedErr struct {
+	err  string
+	real error
+}
+
+func (w *wrappedErr) Error() string { return w.err + ": " + w.real.Error() }
+
+func (w *wrappedErr) Unwrap() error { return w.real }
+
+//Wrap wrap the given error.
+//This does nothing if the error is nil
+func Wrap(str string, err error) error {
+	if err == nil {
+		return nil
+	}
+	return &wrappedErr{err: str, real: err}
+}
